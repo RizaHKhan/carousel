1f84cace0b94860b1cacefe4b58549d39d3a9d78
khanr <khanriza@gmail.com>
2020-06-05 22:55:39 -0400

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,55 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="inspire">
+    <v-container class="fill-height">
+      <v-card
+        class="mx-auto"
+        width="400"
+        height="400"
+        v-for="slide in firstThree"
+        :key="slide.name"
+        :style="{ backgroundImage: 'url(' + slide.img + ')' }"
+      >
+        <v-card-title>{{ slide.name }} </v-card-title>
+      </v-card>
+      <v-row justify="space-around">
+        <v-btn @click="prev">Prev</v-btn>
+        <v-btn @click="next">Next</v-btn>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
+import { mapGetters, mapMutations } from "vuex";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld,
+  computed: {
+    ...mapGetters({
+      slides: "slideGetter"
+    }),
+    firstThree() {
+      return this.slides.slice(0, 3);
+    }
   },
+  methods: {
+    ...mapMutations(["next", "prev"])
+  }
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.v-card {
+  background-size: cover;
+  background-position: center;
+  animation: fade-in 1s ease forwards;
+}
+
+@keyframes fade-in {
+  0% {
+    opacity: 0;
+  }
+
+  100% {
+    opacity: 1;
+  }
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,47 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    slide: [
+      {
+        img: "https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixlib=rb-1.2.1&w=1000&q=80",
+        name: "Ferrary"
+      },
+      {
+        img: "https://officialbespoke.com/wp-content/uploads/2019/09/15052019_New-McLaren-GT_4.jpg",
+        name: "Maclaren"
+      },
+      {
+        img: "https://images.unsplash.com/photo-1546768292-fb12f6c92568?ixlib=rb-1.2.1&w=1000&q=80",
+        name: "Lamborgini"
+      },
+      {
+        img: "https://images.financialexpress.com/2016/09/McLaren-P1-L-620x413.jpg",
+        name: "Aston Martin"
+      },
+      {
+        img:
+          "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/2020-aston-martin-vantage-amr-132-1571762595.jpg?crop=0.697xw:0.784xh;0.303xw,0.216xh&resize=640:*",
+        name: "Cardillac"
+      }
+    ]
+  },
+  getters: {
+    slideGetter: state => state.slide
   },
   mutations: {
+    next(state) {
+      let popped = state.slide.pop();
+      state.slide.unshift(popped);
+    },
+    prev(state) {
+      let shifted = state.slide.shift();
+      state.slide.push(shifted);
+    }
   },
-  actions: {
-  },
-  modules: {
-  },
+  actions: {},
+  modules: {}
 });
