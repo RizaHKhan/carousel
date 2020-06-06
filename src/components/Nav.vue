fb0e02ae1e598093ad143aaefb19f0f8a8fc491e
khanr <khanriza@gmail.com>
2020-06-06 13:38:01 -0400

    added navigation component and drawer controlled from store

---
+++ src/components/Nav.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-toolbar>
+    <v-app-bar-nav-icon @click="changeDrawer"></v-app-bar-nav-icon>
+  </v-toolbar>
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+export default {
+  data() {
+    return {};
+  },
+  methods: {
+    ...mapMutations(["changeDrawer"])
+  }
+};
+</script>
